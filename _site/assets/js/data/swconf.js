30eee72f25da14d104c7d631c0f867bb3b4fce10
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-14 21:03:24 +0100

    L3akCTF 2025 Dumber

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752517650',resources: [
+    cacheName: 'chirpy-1752517712',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
