5ce16d55af95a4264c7c4753477577e2fffd85b2
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-14 19:27:48 +0100

    no favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752515015',resources: [
+    cacheName: 'chirpy-1752517650',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
