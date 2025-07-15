a40a326ef8a63d62b80d905e9af3a38df5cb273b
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-15 17:31:11 +0100

    basic lll

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752549156',resources: [
+    cacheName: 'chirpy-1752549212',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
