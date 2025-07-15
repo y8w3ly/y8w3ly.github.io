6a2275d835ca5d90659b5f21d415a1d8bbd64380
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-15 03:58:57 +0100

    secret squared added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752544899',resources: [
+    cacheName: 'chirpy-1752545157',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
