bc08e7782b09fa9c505f881401d3cb4ed2579ebe
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-15 03:05:36 +0100

    changed icon for L3ak25

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752524672',resources: [
+    cacheName: 'chirpy-1752544899',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
