9cf4a981eca07d57701fd65f4c4bf64c9e5c0591
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-15 04:11:03 +0100

    i hope this works

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752545157',resources: [
+    cacheName: 'chirpy-1752548824',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
