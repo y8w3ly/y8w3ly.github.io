fa50bf3925d3f6a4c7e0cc29053c8bb020fc70c0
Yanis AITOUALI <urmomsfavplayer@haxor10.esi.dz>
2025-07-16 19:27:03 +0100

    small modification madamha tmchi

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752689758',resources: [
+    cacheName: 'chirpy-1752690108',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
